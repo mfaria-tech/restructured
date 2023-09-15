baff193f00cdea70b4f1953ccfc0db0f70a49bbc
Marcus Faria <mfaria150.github@gmail.com>
2023-09-14 23:47:58 -0300

    feat(footer): Added text content for the footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,9 @@
 import styles from "../../style/global.module.css"
 
 export default function Footer() {
-  return <footer className={styles.footer}></footer>
+  return (
+    <footer className={styles.footer}>
+      Created by Marcus Faria
+    </footer>
+  )
 }
