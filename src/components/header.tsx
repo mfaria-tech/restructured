946262dfd8b80e6b900b361f5c23150f79e67086
Marcus Faria <mfaria150.github@gmail.com>
2023-09-14 23:53:11 -0300

    feat(header): Update logotype in header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@ import styles from "../../style/global.module.css"
 export default function Header() {
   return (
     <header className={styles.header}>
-      <>LOGO</>
+      <div className={styles.logo}>Restructured</div>
       <></>
     </header>
   )
