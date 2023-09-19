6854c38bc759dad5172df261ef7f616b48984d5a
Marcus Faria <mfaria150.github@gmail.com>
2023-09-19 20:22:56 -0300

    feat(index): Add Settings element

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,11 @@
+import React from "react"
+import Settings from "../src/components/settings"
+
+
 export default function Page() {
-  return <></>
+  return (
+    <>
+      <Settings />
+    </>
+  )
 }
