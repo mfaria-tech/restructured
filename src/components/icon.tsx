5144f1f67bbcf29b583387f34aa330e3e1032202
Marcus Faria <mfaria150.github@gmail.com>
2023-09-19 20:26:07 -0300

    feat(icon): Add optional properties for icon
    - Add properties for event click
    - Add properties for aditional attributes

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -1,6 +1,15 @@
 import styles from "../../style/global.module.css"
 
-export default function Icon({ icon }: 
-  { icon: string }) {
-    return <span className={styles.icon}>{icon}</span>
+export default function Icon({ icon, click, props }: { 
+  icon: string,
+  click?: Function,
+  props?: any 
+}) {
+  return (
+    <span 
+      className={styles.icon} 
+      onClick={click} {...props} >
+        {icon}
+    </span>
+  )
 }
