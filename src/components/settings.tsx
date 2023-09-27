c172c493b091cdf3bc2e37c4dc52d4f33552a4b5
Marcus Faria <mfaria150.github@gmail.com>
2023-09-27 08:34:11 -0300

    feat(settings): Add tabs role on settings

--- src/components/settings.tsx
+++ src/components/settings.tsx
@@ -5,32 +5,42 @@ import SetTag from "./settag";
 export default function Settings() {
   const ref = useRef(null);
   const [ open, setOpen] = useState("false");
+  const [ enStt, setEnStt ] = useState("");
+  const [ settings, setSettings ] = useState({
+    "font-family": "",
+    "options": {
+      "global": "true",
+      "elements": "false",
+      "icons": "false"
+    }
+  });
 
-  // {
-  //   "font-family": "",
-  //   "elements": {
-  //     "h1": 
-  //   },
-  // };
-  let settings;
-  const enStt = localStorage.getItem("settings");
-  if ( enStt ) {
-    const buf = Buffer.from(enStt, "base64").toString("utf8");
-    settings = JSON.parse(buf);
-  } else {
-    settings= {
-      "font-family": "",
-      "options": {
-        "global": true,
-        "elements": false,
-        "icons": false
-      }
-    };
-  }
+  function updateSettings(obj) {
+    setSettings((settings) => ({
+      ...settings,
+      ...obj
+    }));
+  };
 
   function displaySettings(e) {
     setOpen(e.detail.open ? "true" : "false")
-    console.log("event")
+  }
+
+  function activeTab(id) {
+    Object.keys(settings.options)
+      .forEach((el) => { settings.options[el] = "false" });
+    settings.options[id] = "true";
+    updateSettings(settings);
+  }
+  
+  useEffect(() => {
+    localStorage.getItem("settings");
+  }, [enStt])
+  
+  
+  if ( enStt ) {
+    const buf = Buffer.from(enStt, "base64").toString("utf8");
+    updateSettings(JSON.parse(buf));
   }
 
   useEffect(() => {
@@ -44,36 +54,46 @@ export default function Settings() {
   return (
     <div className={styles.settings} is-open={open} ref={ref} >
       <div className={styles.options}>
-        <span is-selected={settings.options.global} >
+        <span is-selected={settings.options.global} 
+          onClick={() => activeTab("global")} >
           Global
         </span>
-        <span is-selected={settings.options.elements} >
+        <span is-selected={settings.options.elements} 
+          onClick={() => activeTab("elements")} >
           Elements
         </span>
-        <span is-selected={settings.options.icons} >
+        <span is-selected={settings.options.icons} 
+          onClick={() => activeTab("icons")} >
           Icons
         </span>
       </div>
-      <details className={styles.card}>
-        <summary>Heading 1</summary>
-        <SetTag />
-      </details>
-      <details className={styles.card}>
-        <summary>Heading 2</summary>
-        <SetTag />
-      </details>
-      <details className={styles.card}>
-        <summary>Heading 3</summary>
-        <SetTag />
-      </details>
-      <details className={styles.card}>
-        <summary>Heading 4</summary>
-        <SetTag />
-      </details>
-      <details className={styles.card}>
-        <summary>Heading 5</summary>
-        <SetTag />
-      </details>
+      <div className={styles.tab} 
+        tab-name="global" is-open={settings.options.global} >  
+        <details className={styles.card}>
+          <summary>Heading 1</summary>
+          <SetTag />
+        </details>
+        <details className={styles.card}>
+          <summary>Heading 2</summary>
+          <SetTag />
+        </details>
+        <details className={styles.card}>
+          <summary>Heading 3</summary>
+          <SetTag />
+        </details>
+        <details className={styles.card}>
+          <summary>Heading 4</summary>
+          <SetTag />
+        </details>
+        <details className={styles.card}>
+          <summary>Heading 5</summary>
+          <SetTag />
+        </details>
+      </div>
+      <div className={styles.tab}
+        tab-name="elements" is-open={settings.options.elements} ></div>
+      <div className={styles.tab}
+        tab-name="icons" is-open={settings.options.icons} ></div>
     </div>
   )
 }
